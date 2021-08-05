96b22f3de0ce66d3d7f8fcced2af648ad90da3b0
Brandon Tuttle <tuttlebr@ncc-1701.biz>
2021-08-04 23:50:59 -0400

    updating some examples to actually work:

--- src/object-detection.py
+++ src/object-detection.py
@@ -1,20 +1,25 @@
 import autogluon.core as ag
 from autogluon.vision import ImagePredictor
 
-from logging import info 
+from logging import info, basicConfig
 
-basicConfig(format='%(asctime)s %(message)s') 
+basicConfig(format="%(asctime)s %(message)s")
 
-train_dataset, _, test_dataset = ImagePredictor.Dataset.from_folders('https://autogluon.s3.amazonaws.com/datasets/shopee-iet.zip')
+train_dataset, _, test_dataset = ImagePredictor.Dataset.from_folders(
+    "https://autogluon.s3.amazonaws.com/datasets/shopee-iet.zip"
+)
 info(train_dataset)
 
 predictor = ImagePredictor()
 
-predictor.fit(train_dataset, hyperparameters={'epochs': 2})
+predictor.fit(train_dataset, hyperparameters={"epochs": 2})
 fit_result = predictor.fit_summary()
-info('Top-1 train acc: %.3f, val acc: %.3f' %(fit_result['train_acc'], fit_result['valid_acc']))
+info(
+    "Top-1 train acc: %.3f, val acc: %.3f"
+    % (fit_result["train_acc"], fit_result["valid_acc"])
+)
 
-image_path = test_dataset.iloc[0]['image']
+image_path = test_dataset.iloc[0]["image"]
 result = predictor.predict(image_path)
 info(result)
 
